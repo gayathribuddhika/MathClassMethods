25520fbc805bd8c7a89685b867de8c5bced0ca0b
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-04 09:28:37 +0530

    Math Class Methods
    Math Class Methods in Java

---
+++ MathClassMethods.java
@@ -0,0 +1,18 @@
+
+package mathclassmethods;
+
+
+public class MathClassMethods {
+
+    
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-26.5));
+        System.out.println(Math.ceil(8.3 ));
+        System.out.println(Math.floor(8.9 ));
+        System.out.println(Math.max(3.4,6.9));
+        System.out.println(Math.min(8.3,2.9 ));
+        System.out.println(Math.pow(10,4));
+        System.out.println(Math.sqrt(1024 ));
+    }
+    
+}
